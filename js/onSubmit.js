451521e887adc8286356fd1fa604d5973c1f383e
Luseniik <lkromah@aggies.ncat.edu>
2015-04-25 04:18:28 -0400

    Add post to single page
    Added “Add post” area to home page for minimalism

--- js/onSubmit.js
+++ js/onSubmit.js
@@ -1,6 +1,25 @@
 var addPost =function(){
 
 	var x = document.forms;
-	alert(x[0]['post'].value);
+	var post=x[0]['post'].value;
+	var email=x[0]['inputEmail'].value;
+	var email=x[0]['inputEmail'].value;
+	alert();
+
+	
 
 };
+
+function addRow() {
+    var div = document.createElement('div');
+
+    div.className = 'row';
+
+    div.innerHTML = '<input type="text" name="name" value="" />\
+        <input type="text" name="value" value="" />\
+        <label> <input type="checkbox" name="check" value="1" /> Checked? </label>\
+        <input type="button" value="-" onclick="removeRow(this)">';
+
+     document.getElementById('content').appendChild(div);
+}
+
